333d36723556689638ab0e6ac3400a583332a6d6
Kamrul <kamrulfci2014@gmail.com>
2023-09-22 20:21:33 +0600

    expenses-tracker complete

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php 
-
-echo "Welcome to PHP-Laravel-Career-Path";
+<?php
+  require_once '../app/controllers/MainCotroller.php';
+  require_once '../app/controllers/IncomeController.php';
+  require_once '../app/controllers/ExpenseController.php';
+  require_once '../app/controllers/CategoryController.php';
+  
+  
+  $app = new MainController();
+  $app->run();
+?>
